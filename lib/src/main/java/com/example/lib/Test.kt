96bf02666f242b3f2c7dbb69c22d9f5e713222f4
Ivan <d1ram.komirol@gmail.com>
2025-10-07 11:30:49 +0200

    "In work"

--- lib/src/main/java/com/example/lib/Test.kt
+++ lib/src/main/java/com/example/lib/Test.kt
@@ -3,39 +3,38 @@ package com.example.lib
 import io.github.serpro69.kfaker.Faker;
 import kotlin.random.Random
 
-fun generateID(i : Double) : Double{
-    return Random.nextDouble(0.0,100.0);
-}
-
-fun showExceptions(message : String){
-    val except = ExceptionsPrimer();
-
-    try { // 1 IllegalArgumentException. require does not meets it conditions)
-        except.ExceptionOne(-1);
-    } catch (e : IllegalArgumentException){
-        println("Catched Exception IllegalArgumentException:\n" + e.toString());
+fun firstTask(){
+    fun generateID(i : Double) : Double{
+        return Random.nextDouble(0.0,100.0);
     }
 
-    try { // 2 IllegalStateException. check meets false, throws exception)
-        except.ExceptionTwo(false);
-    } catch (e : IllegalStateException){
-        println("Catched Exception IllegalStateException:\n" + e.toString());
-    }
+    fun showExceptions(message : String){
+        val except = ExceptionsPrimer();
 
-    try { // 3 ArithmeticException. divide by zero)
-        except.ExceptionThree(200);
-    } catch (e : ArithmeticException){
-        println("Catched Exception ArithmeticException:\n" + e.toString());
-    }
+        try { // 1 IllegalArgumentException. require does not meets it conditions)
+            except.ExceptionOne(-1);
+        } catch (e : IllegalArgumentException){
+            println("Catched Exception IllegalArgumentException:\n" + e.toString());
+        }
 
-    try {
-        except.ExceptionFour(message);
-    } catch (e : BankIsFullException){
-        println("Catched Exception MyException:\n" + e.toString());
-    }
-}
+        try { // 2 IllegalStateException. check meets false, throws exception)
+            except.ExceptionTwo(false);
+        } catch (e : IllegalStateException){
+            println("Catched Exception IllegalStateException:\n" + e.toString());
+        }
 
-fun main(){
+        try { // 3 ArithmeticException. divide by zero)
+            except.ExceptionThree(200);
+        } catch (e : ArithmeticException){
+            println("Catched Exception ArithmeticException:\n" + e.toString());
+        }
+
+        try {
+            except.ExceptionFour(message);
+        } catch (e : BankIsFullException){
+            println("Catched Exception MyException:\n" + e.toString());
+        }
+    }
     val faker = Faker();
 
     var bank = Bank();
@@ -51,4 +50,8 @@ fun main(){
     println(bank.toString());
 
     showExceptions(faker.southPark.quotes());
+}
+
+fun main(){
+
 }
